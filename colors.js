d97accc1e4e597820fdb5a7cc18cad155a47d471
alex-weber98 <65651039+alex-weber98@users.noreply.github.com>
2020-05-20 10:29:45 +0200

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,20 @@
+const colors = {
+    bertaBlue: "#4BDAFF",
+    black: "#000000",
+    bloodOrgan: "#630E10",
+    mango: "#FFA628",
+    nero: "#262626",
+    schoolBusYellow: "#ffd800",
+    lightgreen: "#c0ed8a",
+    darkgreen:"#86b849"
+};
+export default {
+    primaryBackground: colors.mango,
+    secondaryBackground: colors.bertaBlue,
+    primaryText: colors.black,
+    secondaryText: colors.nero,
+    primaryButtonBackground: colors.bloodOrgan,
+    primaryButtonText: colors.schoolBusYellow,
+    fieldLight: colors.lightgreen,
+    fieldDark: colors.darkgreen
+};
